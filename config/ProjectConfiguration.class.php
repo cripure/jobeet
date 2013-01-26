0f5f80b84c6c3992503f563c6eadc4e8da4fd9e4
cripure <notrecripure@gmail.com>
2013-01-26 21:46:59 +0900

    after 1st day finished .

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once '/Applications/MAMP/bin/php/php5.3.6/lib/php/symfony/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
